858a8b3e00ef9152223a183c62402350e80a4cac
Vitor Hugo <vitorhugo0031@outlook.com>
2024-08-27 13:28:52 -0300

    feat: adc content page

---
+++ src/components/ImageSobreNos/index.jsx
@@ -0,0 +1,29 @@
+import './imageSobreNos.css';
+import  banner from '../../imagens/historia/bannerHistoria.png';
+import {  CImage } from '@coreui/react';
+
+export default function ImageSobreNos() {
+
+    return (
+        <section className='sobreNos'>
+
+            <article>
+                <h1>Nossa História</h1>
+                <p>A Que Delícia é uma empresa familiar que teve sua origem em 1980.</p>
+                <p>Desde então a Que Delícia vem se destacando no mercado com agilidade de entrega de mercadorias.
+                </p>
+                <p>
+                    Priorizando manter sempre produtos de qualidade, preços competitivos e excelência no atendimento ao cliente.
+                </p>
+                <p>Considerando-se assim rapidamente no mercado, passando a atender não só
+                    o pequeno, mas também o médio e o grande varejista.
+                </p>
+                <p>Com uma filosofia simples de trabalho, a empresa conta hoje com uma equipe
+                    de colaboradores de alto padrão profissional, oferecendo um completo suporte.
+                </p>
+
+            </article>
+            <CImage fluid src={banner}/>
+        </section>
+    )
+}
