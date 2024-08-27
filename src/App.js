a4a72e086bdf9572baba0dcf0dfc938a46fa4326
Vitor Hugo <vitorhugo0031@outlook.com>
2024-08-27 11:38:48 -0300

    feat: adc component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-
+import "./app.css";
 import BannerLogo from './components/BannerHome';
+import DescriIcons from './components/Descri-icons';
 import RoutsPage from './router';
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
       <RoutsPage/>
 
       <BannerLogo/>
+
+      <DescriIcons/>
     
     
     </div>
