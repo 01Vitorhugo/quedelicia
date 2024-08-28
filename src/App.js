09b314ad8d9ceb61a2d38c5a2255e3759acc0114
Vitor Hugo <vitorhugo0031@outlook.com>
2024-08-27 22:29:55 -0300

    feat: adc button whats

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./app.css";
 import BannerLogo from './components/BannerHome';
 import DescriIcons from './components/Descri-icons';
 import ImageSobreNos from "./components/ImageSobreNos";
+import Estrutura from "./components/PageEstrutura";
 import RoutsPage from './router';
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
       <DescriIcons/>
 
       <ImageSobreNos/>
+
+      <Estrutura/>
     
     
     </div>
