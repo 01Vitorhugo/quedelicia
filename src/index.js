f2d3fe15559ac6d03a57ccb3a8bc85223e7b158e
Vitor Hugo <vitorhugo0031@outlook.com>
2024-09-03 11:20:38 -0300

    delete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
-// import '@coreui/coreui/dist/css/coreui.min.css';
 import App from './App';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
