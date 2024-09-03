ad0df833e4a525c63e3c025c7354310fac3f79ff
Vitor Hugo <vitorhugo0031@outlook.com>
2024-09-03 11:21:29 -0300

    feat: creating component Atendimento

---
+++ src/components/Atendimentos/index.jsx
@@ -0,0 +1,24 @@
+import './atendimento.css';
+
+export default function Atendimento(){
+
+    return(
+        <section className='atendimento'>
+            <article>
+                <h1>Atendemos Food Service</h1>
+            </article>
+            <div className='atendimento-icons'>
+
+                <div className='icon-box1'>
+
+                </div>
+
+                <div className='icon-box2'>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+}
