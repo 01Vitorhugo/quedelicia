f93bb7891f6c5fd03d380970825353df19382160
Vitor Hugo <vitorhugo0031@outlook.com>
2024-09-16 10:12:47 -0300

    delete

--- src/Pages/Produtos/index.jsx
+++ src/Pages/Produtos/index.jsx
@@ -1,4 +1,4 @@
-import './pageProdutos.css'
+
 import { Card } from "flowbite-react";
 import back from '../Produtos/imagens/backgroundBovina.jpg'
 
@@ -29,15 +29,10 @@ export default function PageProdutos() {
                                 imgSrc={list.back}
                             >
                                 <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    Noteworthy technology acquisitions 2021
+                                    Carne Suina
                                 </h5>
-                                <p className="font-normal text-gray-700 dark:text-gray-400">
-                                    Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-                                </p>
                             </Card>
-
-                         
-
+                        
                         </div>
                     </section>
                 )
