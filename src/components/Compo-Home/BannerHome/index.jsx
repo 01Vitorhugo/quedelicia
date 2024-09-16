b6779faff27992f9cdda801ba2e25c9831dfbe89
Vitor Hugo <vitorhugo0031@outlook.com>
2024-09-16 10:13:38 -0300

    adjusting to css

--- src/components/Compo-Home/BannerHome/index.jsx
+++ src/components/Compo-Home/BannerHome/index.jsx
@@ -8,14 +8,13 @@ export default function BannerLogo() {
 
     return (
 
-        <section className="imgageCarousel">
-            <Carousel>
-                <img src={banner2} alt="Banner Atendimento" id="imageCarousel"/>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." />
+        <section className="imgageCarousel" >
+            <Carousel className="car">
+                <img src={banner2} alt="Banner Atendimento" id="imageCarousel" style={{ borderRadius: '0' }}/>
+                <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." style={{ borderRadius: '0' }}/>
+                <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." style={{ borderRadius: '0' }}/>
+                <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." style={{ borderRadius: '0' }}/>
             </Carousel>
-
 
         </section>
 
