805dcefbd90125ec88a79e6d9750433c7649d8a9
pigs <hiury1315ajac@gmail.com>
2024-09-17 23:05:22 -0300

    new style for bottom

--- src/components/Compo-Home/Produtos/index.jsx
+++ src/components/Compo-Home/Produtos/index.jsx
@@ -103,17 +103,21 @@ export default function Produtos() {
           </div>
         </Slider>
       </div>
-
-      <button className="button-verMais">
-      <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
-        <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+      <button className="button-view">
+      <svg xmlns="http://www.w3.org/2000/svg" className="arr-2" viewBox="0 0 24 24">
+        <path
+          d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+        ></path>
       </svg>
-      <span className="text">Veja todos produtos</span>
+      <span className="text">Veja mais produtos</span>
       <span className="circle"></span>
-      <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
-        <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+      <svg xmlns="http://www.w3.org/2000/svg" className="arr-1" viewBox="0 0 24 24">
+        <path
+          d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+        ></path>
       </svg>
     </button>
+       
     </section>
   );
 }
