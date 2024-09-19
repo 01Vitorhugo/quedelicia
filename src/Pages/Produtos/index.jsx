76726ec7f8054827f303cb632fbe73acc7cbae48
Vitor Hugo <vitorhugo0031@outlook.com>
2024-09-19 11:38:40 -0300

    feat: adc arrays to product

--- src/Pages/Produtos/index.jsx
+++ src/Pages/Produtos/index.jsx
@@ -1,44 +1,159 @@
+import './pageProdutos.css';
 
-import { Card } from "flowbite-react";
-import back from '../Produtos/imagens/backgroundBovina.jpg'
-
+import { Card, Spinner } from "flowbite-react";
+import backBovina from '../Produtos/imagens/backgroundBovina.jpg'
+import backSuina from '../Produtos/imagens/backgroundSuina.jpg'
+import backFrango from '../Produtos/imagens/backgroundFrango.jpg'
+import backLactio from '../Produtos/imagens/backgroundLactio.jpg'
+import backPeixe from '../Produtos/imagens/backgroundPeixe.jpg'
+import { useState, useEffect } from 'react';
 
 export default function PageProdutos() {
 
-    const produtosSuinos = [
-        { name: 'Suinos', back: back}
-     
-    ]
+    const [loading, setLoading] = useState(true);
+    const [frango, setFrango] = useState([]);
+    const [peixe, setPeixe] = useState([]);
+    const [lactio, setLactio] = useState([]);
+    const [suino, setSuino] = useState([]);
+    const [bovino, setBovino] = useState([]);
+
+
+    const Produtos = async () => {
+        try {
+            setLoading(true);
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            const prodFrango = [
+                { name: 'Frangos', back: backFrango }
+            ];
+            setFrango(prodFrango);
+
+            const prodPeixe = [
+                { name: 'Peixes', back: backPeixe }
+            ];
+            setPeixe(prodPeixe);
+
+            const prodLactio = [
+                { name: 'Lactios', back: backLactio }
+            ];
+            setLactio(prodLactio);
+
+            const prodBovino = [
+                { name: 'Bovinos', back: backBovina }
+            ];
+            setBovino(prodBovino);
+
+            const prodSuino = [
+                { name: 'Suinos', back: backSuina }
+            ];
+            setSuino(prodSuino);
+
+        } catch (error) {
+            // setError('Ocorreu um erro ao carregar os produtos.');
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        Produtos();
+    }, []);
+
+
+    if (loading) {
+        return <div className='loading'>
+            <Spinner aria-label="Extra large spinner example" size="xl" />
+        </div>;
+    }
+
 
     return (
         <section className="pageProdutos">
-            <h1>Produtos</h1>
 
-            {produtosSuinos.map((list) => {
+            <article>
+                <h1>Nossos Produtos</h1>
+            </article>
+
+            {frango.map((list) => {
                 return (
-                    <section className='listProdutos'>
-                        <div className='background'>
-                            <img src={list.back} alt="Foto Capa" />
-                            <article>{list.name}</article>
-                        </div>
+                    <>
+                        <section className='boxProdutos'>
+                            <figure>
+                                <img src={list.back} alt="Capa Frango" />
 
-                        <div className='cardProdutos'>
-                            <Card
-                                className="max-w-sm"
-                                imgAlt="Meaningful alt text for an image that is not purely decorative"
-                                imgSrc={list.back}
-                            >
-                                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                    Carne Suina
-                                </h5>
-                            </Card>
-                        
-                        </div>
-                    </section>
+                                <article>{list.name}</article>
+                            </figure>
+
+                        </section>
+                    </>
+
                 )
-
-
             })}
+
+            {peixe.map((list) => {
+                return (
+                    <>
+                        <section className='boxProdutos'>
+                            <figure>
+                                <img src={list.back} alt="Capa Frango" />
+
+                                <article>{list.name}</article>
+                            </figure>
+
+                        </section>
+                    </>
+
+                )
+            })}
+
+            {bovino.map((list) => {
+                return (
+                    <>
+                        <section className='boxProdutos'>
+                            <figure>
+                                <img src={list.back} alt="Capa Frango" />
+
+                                <article>{list.name}</article>
+                            </figure>
+
+                        </section>
+                    </>
+
+                )
+            })}
+
+            {suino.map((list) => {
+                return (
+                    <>
+                        <section className='boxProdutos'>
+                            <figure>
+                                <img src={list.back} alt="Capa Frango" />
+
+                                <article>{list.name}</article>
+                            </figure>
+
+                        </section>
+                    </>
+
+                )
+            })}
+
+            {lactio.map((list) => {
+                return (
+                    <>
+                        <section className='boxProdutos'>
+                            <figure>
+                                <img src={list.back} alt="Capa Frango" />
+
+                                <article>{list.name}</article>
+                            </figure>
+
+                        </section>
+                    </>
+
+                )
+            })}
+
 
         </section>
     )
