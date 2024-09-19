b296575aaa8f90234e0ba1f0e0101a31764a25b1
Vitor Hugo <vitorhugo0031@outlook.com>
2024-09-18 22:10:14 -0300

    adjusting

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import Atendimento from "../../components/Compo-Home/Atendimentos";
 import BannerLogo from "../../components/Compo-Home/BannerHome";
 import DescriIcons from "../../components/Compo-Home/Descri-icons";
-import Estrutura from "../../components/Compo-Home/PageEstrutura";
+import Estrutura from "../../components/Compo-Home/Estrutura";
 import Parceiros from "../../components/Compo-Home/Parceiros";
 import Produtos from "../../components/Compo-Home/Produtos";
 
