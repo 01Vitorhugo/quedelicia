871d05a1af057926321057995aaff0968c0d913a
Vitor Hugo <vitorhugo0031@outlook.com>
2024-09-20 11:56:12 -0300

    test

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -12,8 +12,8 @@ export default function Home(){
         <BannerLogo/>
         <DescriIcons/>
         <Produtos/>
-        <Atendimento/>
         <Estrutura/>
+        <Atendimento/>
         <Parceiros/>
         </>
     )
