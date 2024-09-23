6870a8a236b37b26f07fe1b58ee466dd612c3112
Vitor Hugo <vitorhugo0031@outlook.com>
2024-09-22 23:05:03 -0300

    test

--- src/components/Compo-Home/BannerHome/index.jsx
+++ src/components/Compo-Home/BannerHome/index.jsx
@@ -10,10 +10,13 @@ export default function BannerLogo() {
 
         <section className="imgageCarousel" >
             <Carousel className="car">
-                <img src={banner2} alt="Banner Atendimento" id="imageCarousel" style={{ borderRadius: '0' }}/>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." style={{ borderRadius: '0' }}/>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." style={{ borderRadius: '0' }}/>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." style={{ borderRadius: '0' }}/>
+                <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
+                <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
+                <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
+                <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
+                {/* <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+                <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+                <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." /> */}
             </Carousel>
 
         </section>
