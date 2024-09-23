8b748ce56542d17772613d2111caec5e7f88e1c7
pigs <hiury1315ajac@gmail.com>
2024-09-23 14:33:48 -0300

    adicionando links for bottom and footer

--- src/components/Compo-Home/Produtos/index.jsx
+++ src/components/Compo-Home/Produtos/index.jsx
@@ -8,6 +8,7 @@ import Queijo from "../../../imagens/produtos/queijo.png";
 import Tilapia from "../../../imagens/produtos/tilapia.png";
 import Batata from "../../../imagens/produtos/batata.png";
 import Slider from "react-slick";
+import {Link} from "react-router-dom";
 
 export default function Produtos() {
   var settings = {
@@ -103,21 +104,26 @@ export default function Produtos() {
           </div>
         </Slider>
       </div>
-      <button className="button-view">
-      <svg xmlns="http://www.w3.org/2000/svg" className="arr-2" viewBox="0 0 24 24">
-        <path
-          d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-        ></path>
-      </svg>
-      <span className="text">Veja mais produtos</span>
-      <span className="circle"></span>
-      <svg xmlns="http://www.w3.org/2000/svg" className="arr-1" viewBox="0 0 24 24">
-        <path
-          d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-        ></path>
-      </svg>
-    </button>
-       
+      <Link to="/produtos">        
+        <button className="button-view">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="arr-2"
+            viewBox="0 0 24 24"
+          >
+            <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+          </svg>
+          <span className="text">Veja mais produtos</span>
+          <span className="circle"></span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="arr-1"
+            viewBox="0 0 24 24"
+          >
+            <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
+          </svg>
+        </button>
+      </Link>
     </section>
   );
 }
