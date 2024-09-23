ef233f6a806443f4d7882563359e47caedb74b7d
Vitor Hugo <vitorhugo0031@outlook.com>
2024-09-23 10:07:34 -0300

    text delete

--- src/components/Compo-Home/BannerHome/index.jsx
+++ src/components/Compo-Home/BannerHome/index.jsx
@@ -14,9 +14,6 @@ export default function BannerLogo() {
                 <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
                 <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
                 <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
-                {/* <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
-                <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." /> */}
             </Carousel>
 
         </section>
