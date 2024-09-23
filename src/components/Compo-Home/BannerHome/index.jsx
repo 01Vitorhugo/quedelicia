da10c8f743482fe0f5f18006988b3d9f2899f623
Vitor Hugo <vitorhugo0031@outlook.com>
2024-09-23 09:57:55 -0300

    feat: adjusting

--- src/components/Compo-Home/BannerHome/index.jsx
+++ src/components/Compo-Home/BannerHome/index.jsx
@@ -1,53 +1,22 @@
 import "./bannerHome.css";
 import banner2 from "../../../imagens/teste2.png"
-import banner22 from "../../../imagens/teste22.png"
 import { Carousel } from "flowbite-react";
-import React, { useState, useEffect } from 'react';
 
 
 export default function BannerLogo() {
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-    const images = [banner22, banner2, banner22, banner2];
-
-    useEffect(() => {
-        const handleResize = () => {
-            setScreenWidth(window.innerWidth);
-        };
-
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-
 
 
     return (
 
         <section className="imgageCarousel" >
             <Carousel className="car">
-
-                {screenWidth <= 756 ? (
-                    <>
-                        {images.map((image, index) => (
-                            <img key={index} src={image} alt="Banner Atendimento" id="imageCarousel" />
-                        ))}
-                    </>
-                ) : (
-                    <>
-                        <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
-                        <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
-                        <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
-                        <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
-
-                        <img src={banner22} alt="Banner Atendimento" id="imageCarousel" />
-                        <img src={banner22} alt="Banner Atendimento" id="imageCarousel" />
-                        <img src={banner22} alt="Banner Atendimento" id="imageCarousel" />
-                        <img src={banner22} alt="Banner Atendimento" id="imageCarousel" />
-                    </>
-                )}
+                <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
+                <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
+                <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
+                <img src={banner2} alt="Banner Atendimento" id="imageCarousel" />
+                {/* <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..." />
+                <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..." />
+                <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..." /> */}
             </Carousel>
 
         </section>
