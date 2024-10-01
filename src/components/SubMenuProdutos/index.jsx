2fce3cba6fd221d339390276b3ab5266ab0eaafe
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-01 11:33:55 -0300

    feat: add subMenu

---
+++ src/components/SubMenuProdutos/index.jsx
@@ -0,0 +1,24 @@
+import './subMenu.css'
+
+
+export default function SubMenu(){
+
+
+    return(
+       <nav className='subMenu'>
+        <ul>
+            <li>Aves</li>
+            <li>Bovinos</li>
+            <li>Suinos</li>
+            <li>Frios e Laticínios</li>
+            <li>Paixes</li>
+            <li>Conservas e secos</li>
+            <li>Temperos</li>
+            <li>Feijão</li>
+            <li>Feijoada</li>
+
+        </ul>
+
+       </nav>
+    )
+}
