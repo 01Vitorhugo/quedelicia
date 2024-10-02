2310a39845766a389787f330c18941dba12efdf9
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-02 10:41:50 -0300

    adc routes page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,6 +2,14 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Menu from "../components/Navbar";
 import PageProdutos from "../Pages/Produtos";
 import Home from "../Pages/Home";
+import Aves from "../Pages/Produtos/Page-Aves";
+import Congelados from "../Pages/Produtos/Page-Congelados";
+import Conservas from "../Pages/Produtos/Page-Conservas";
+import Bovinos from "../Pages/Produtos/Page-CortesBovinos";
+import Feijoada from "../Pages/Produtos/Page-Feijoada";
+import Frios from "../Pages/Produtos/Page-Frios";
+import Peixes from "../Pages/Produtos/Page-Peixes";
+import Suinos from "../Pages/Produtos/Page-Suinos";
 
 export default function RoutsPage() {
 
@@ -11,7 +19,15 @@ export default function RoutsPage() {
 
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="/produtos" element={ <PageProdutos/>} />
+                <Route path="/produtos" element={<PageProdutos/> } />
+                <Route path="/produtos/aves" element={ <Aves/> } />
+                <Route path="/produtos/congelados" element={<Congelados/> } />
+                <Route path="/produtos/conservas" element={<Conservas/> } />
+                <Route path="/produtos/bovinos" element={<Bovinos/> } />
+                <Route path="/produtos/feijoada" element={ <Feijoada/> } />
+                <Route path="/produtos/frios" element={<Frios/> } />
+                <Route path="/produtos/peixes" element={ <Peixes/>} />
+                <Route path="/produtos/suinos" element={ <Suinos/>} />
                 
             </Routes>
         </BrowserRouter>
