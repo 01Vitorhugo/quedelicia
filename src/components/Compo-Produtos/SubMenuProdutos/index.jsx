8b32f0ef8b29e89d41f541f66e0cac4147bb7604
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-02 11:39:51 -0300

    adjusting the bug

--- src/components/Compo-Produtos/SubMenuProdutos/index.jsx
+++ src/components/Compo-Produtos/SubMenuProdutos/index.jsx
@@ -8,14 +8,14 @@ export default function SubMenu(){
     return(
        <nav className='subMenu'>
         <ul>
-            <li><Link to='produtos/aves'>Aves</Link></li>
-            <li><Link to='produtos/bovinos'>Cortes bovinos</Link></li>
-            <li><Link to='produtos/suinos'>Suinos</Link></li>
-            <li><Link to='produtos/frios'>Frios e laticínios</Link></li>
-            <li><Link to='produtos/congelados'>Congelados</Link></li>
-            <li><Link to='produtos/peixes'>Peixes</Link></li>
-            <li><Link to='produtos/conservas'>Conservas e secos</Link></li>
-            <li><Link to='produtos/feijoada'>Pertences de feijoada</Link></li>
+            <li><Link to='/produtos/aves' title='Aves'>Aves</Link></li>
+            <li><Link to='/produtos/bovinos' title='Cortes bovinos'>Cortes bovinos</Link></li>
+            <li><Link to='/produtos/suinos' title='Suinos'>Suinos</Link></li>
+            <li><Link to='/produtos/frios' title='Frios e laticínios'>Frios e laticínios</Link></li>
+            <li><Link to='/produtos/congelados' title='Congelados'>Congelados</Link></li>
+            <li><Link to='/produtos/peixes' title='Peixes'>Peixes</Link></li>
+            <li><Link to='/produtos/conservas' title='Conservas e secos'>Conservas e secos</Link></li>
+            <li><Link to='/produtos/feijoada' title='Pertences de feijoada'>Pertences de feijoada</Link></li>
         </ul>
 
        </nav>
