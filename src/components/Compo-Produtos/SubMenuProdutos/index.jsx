8d1de70712e41e13895c4fa252bbd0ad2117c63a
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-06 01:57:01 -0300

    N/A

--- src/components/Compo-Produtos/SubMenuProdutos/index.jsx
+++ src/components/Compo-Produtos/SubMenuProdutos/index.jsx
@@ -19,42 +19,42 @@ export default function SubMenu() {
                 <Link to='/produtos/aves' title='Aves'>
                     <li>
                         <p>Aves</p>
-                        <img src={ave} alt="icone ave" />
+                        <img src={ave} alt="icone ave" id='icons-submenu'/>
                     </li>
                 </Link>
 
                 <Link to='/produtos/bovinos' title='Cortes bovinos'>
                     <li>
                         <p>Cortes bovinos</p>
-                        <img src={bovino} alt="icone cortes bovinos" />
+                        <img src={bovino} alt="icone cortes bovinos" id='icons-submenu'/>
                     </li>
                 </Link>
 
                 <Link to='/produtos/suinos' title='Suinos'>
                     <li>
                         <p>Suinos</p>
-                        <img src={suino} alt="icone suinos" />
+                        <img src={suino} alt="icone suinos" id='icons-submenu'/>
                     </li>
                 </Link>
 
                 <Link to='/produtos/frios' title='Frios e laticínios'>
                     <li>
                         <p>Frios e laticínios</p>
-                        <img src={frios} alt="icone frios e laticínios" />
+                        <img src={frios} alt="icone frios e laticínios" id='icons-submenu'/>
                     </li>
                 </Link>
 
                 <Link to='/produtos/congelados' title='Congelados'>
                     <li>
                         <p>Congelados</p>
-                        <img src={congelados} alt="icone congelados" />
+                        <img src={congelados} alt="icone congelados" id='icons-submenu'/>
                     </li>
                 </Link>
 
                 <Link to='/produtos/peixes' title='Peixes'>
                     <li>
                         <p>Peixes</p>
-                        <img src={peixes} alt="icone peixes" />
+                        <img src={peixes} alt="icone peixes" id='icons-submenu'/>
                     </li>
                 </Link>
 
@@ -62,14 +62,14 @@ export default function SubMenu() {
                 <Link to='/produtos/conservas' title='Conservas e secos'>
                     <li>
                         <p>Conservas e secos</p>
-                        <img src={conservas} alt="icone conservas e secos" />
+                        <img src={conservas} alt="icone conservas e secos" id='icons-submenu'/>
                     </li>
                 </Link>
 
                 <Link to='/produtos/feijoada' title='Pertences de feijoada'>
                     <li>
                         <p>Pertences de feijoada</p>
-                        <img src={feijoada} alt="icone feijoada" />
+                        <img src={feijoada} alt="icone feijoada" id='icons-submenu'/>
                     </li>
                 </Link>
             </ul>
