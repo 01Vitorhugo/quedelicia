b2016f23456c9289364ac6a7ff00a5d840f0a603
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-07 00:57:24 -0300

    feat: creat component

--- src/Pages/Produtos/Page-Aves/index.jsx
+++ src/Pages/Produtos/Page-Aves/index.jsx
@@ -1,14 +1,47 @@
 import SubMenu from '../../../components/Compo-Produtos/SubMenuProdutos'
 import './aves.css'
+import logo from '../../../imagens/icons-produtos-submenu/ave.svg'
+import data from '../../../components/Compo-Produtos/CardProduto/data'
 
 
 export default function Aves() {
+
+    const avesProducts = data.cardData.filter(
+        (item) => item.category === 'aves'
+    );
 
     return (
         <>
             <SubMenu />
             <section className='aves'>
-                <h1>Aves</h1>
+                <article>
+                    <h1>Aves</h1>
+                    <figure>
+                        <img src={logo} alt="logo ave" />
+                    </figure>
+                </article>
+
+                <div className='box-produto-aves'>
+                    {
+                        avesProducts.map((item) => {
+                            return (
+                                <section className='box-produto'>
+                                    <figure>
+                                        <img src={item.img} alt="imagem produto" />
+                                    </figure>
+
+                                    <article>
+                                        <p>{item.title}</p>
+
+                                    </article>
+                                </section>
+
+                            )
+
+                        })
+                    }
+
+                </div>
             </section>
         </>
     )
