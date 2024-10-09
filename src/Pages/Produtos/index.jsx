26f972b7c13203c50d6dae83eb4ec3a5b3514f0a
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-09 11:13:48 -0300

    adjusting to component

--- src/Pages/Produtos/index.jsx
+++ src/Pages/Produtos/index.jsx
@@ -1,12 +1,9 @@
 import "./pageProdutos.css";
 import SubMenu from "../../components/Compo-Produtos/SubMenuProdutos";
 
-import aves from '../../imagens/icons-produtos-submenu/ave.svg'
-import aves1 from '../../imagens/aves/Filé-de-Frango.png'
-import aves2 from '../../imagens/aves/frango-inteiro.png'
-import aves3 from '../../imagens/aves/file-de-coxa.png'
-import aves4 from '../../imagens/aves/meio-da-asa.png'
-import { Link } from "react-router-dom";
+
+import CompoAves from "../../components/Compo-Produtos/CompoAves";
+import CompoBovinos from "../../components/Compo-Produtos/CompoBovinos";
 
 export default function PageProdutos() {
 
@@ -18,61 +15,10 @@ export default function PageProdutos() {
           <h1>Nossos Produtos</h1>
         </article>
 
-        <div className="container-produtos">
-          <article className="text">
-            <img src={aves} alt="icone ave" />
-            <h1>Aves</h1>
-          </article>
+        <CompoAves/>
+        <CompoBovinos/>
 
-          <section className="box-produto">
-            <section className="container">
-
-              <div className="card-produto">
-                <figure>
-                  <img src={aves1} alt="imagem aves" />
-                </figure>
-                <article>
-                  <p>Filé de Frango (peito s/ osso)</p>
-                </article>
-              </div>
-
-              <div className="card-produto">
-                <figure>
-                  <img src={aves2} alt="imagem aves" />
-                </figure>
-                <article>
-                  <p>Filé de Frango (peito s/ osso)</p>
-                </article>
-              </div>
-
-              <div className="card-produto">
-                <figure>
-                  <img src={aves3} alt="imagem aves" />
-                </figure>
-                <article>
-                  <p>Filé de Frango (peito s/ osso)</p>
-                </article>
-              </div>
-
-              <div className="card-produto">
-                <figure>
-                  <img src={aves4} alt="imagem aves" />
-                </figure>
-                <article>
-                  <p>Filé de Frango (peito s/ osso)</p>
-                </article>
-              </div>
-
-            </section>
-
-            <div className="button">
-              <button>
-                <p><Link to='/produtos/aves'>Veja Mais</Link></p>
-                </button>
-            </div>
-
-          </section>
-        </div>
+       
 
 
       </section>
