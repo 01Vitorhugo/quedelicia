fcb38fdca0a109e21dd1e362049f210287c174de
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-11 12:04:51 -0300

    adc key

--- src/Pages/Produtos/Page-CortesBovinos/index.jsx
+++ src/Pages/Produtos/Page-CortesBovinos/index.jsx
@@ -22,7 +22,7 @@ export default function Bovinos(){
                     {
                         bovinosProducts.map((item) => {
                             return (
-                                <section className='box-produto'>
+                                <section className='box-produto' key={item.id}>
                                     <figure>
                                         <img src={item.img} alt="imagem produto" />
                                     </figure>
