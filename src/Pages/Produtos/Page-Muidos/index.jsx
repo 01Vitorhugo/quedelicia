642b54f2a05aab3ee157a3f7ecba905b1c8e3706
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-11 11:21:16 -0300

    new page

---
+++ src/Pages/Produtos/Page-Muidos/index.jsx
@@ -0,0 +1,16 @@
+import SubMenu from "../../../components/Compo-Produtos/SubMenuProdutos";
+import './muidos.css'
+
+
+export default function Muidos(){
+
+    return(
+        <>
+        <SubMenu/>
+        <section className='muidos'>
+            <h1>Muidos</h1>
+
+        </section>
+    </>
+    )
+}
