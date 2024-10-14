e3f03739909f1056ca33fb21baf319b8562ad7ca
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-13 21:24:42 -0300

    new component

---
+++ src/components/Compo-Produtos/CompoCongelados/index.jsx
@@ -0,0 +1,72 @@
+import { Link } from 'react-router-dom'
+import congelados from '../../../imagens/icons-produtos-submenu/congelados.svg'
+import congelados1 from '../../../imagens/congelados/Couve flor.png'
+import congelados2 from '../../../imagens/congelados/anel de cebola.png'
+import congelados3 from '../../../imagens/congelados/batata bem brasil.png'
+import congelados4 from '../../../imagens/congelados/batata lamb weston.png'
+import './compoCongelados.css'
+
+
+export default function CompoCongelados() {
+    return (
+        <div className="container-produtosCongelados">
+            <article className="text">
+                <img src={congelados} alt="icone congelados" />
+                <h1>Congelados</h1>
+            </article>
+
+            <section className="box-produto">
+                <section className="container">
+
+                    <div className="card-produto">
+                        <figure>
+                            <img src={congelados1} alt="imagem congelados" />
+                        </figure>
+                        <article>
+                            <p>Filé de Frango (peito s/ osso)</p>
+                        </article>
+                    </div>
+
+                    <div className="card-produto">
+                        <figure>
+                            <img src={congelados2} alt="imagem congelados" />
+                        </figure>
+                        <article>
+                            <p>Filé de Frango (peito s/ osso)</p>
+                        </article>
+                    </div>
+
+                    <div className="card-produto">
+                        <figure>
+                            <img src={congelados3} alt="imagem congelados" />
+                        </figure>
+                        <article>
+                            <p>Filé de Frango (peito s/ osso)</p>
+                        </article>
+                    </div>
+
+                    <div className="card-produto">
+                        <figure>
+                            <img src={congelados4} alt="imagem congelados" />
+                        </figure>
+                        <article>
+                            <p>Filé de Frango (peito s/ osso)</p>
+                        </article>
+                    </div>
+
+                </section>
+
+                <div className="button">
+                    <button>
+                        <p><Link to='/produtos/congelados'>Veja Mais</Link></p>
+                        
+                    </button>
+                </div>
+
+            </section>
+        </div>
+
+    )
+
+}
+
