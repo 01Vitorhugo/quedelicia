975086b6c0a885b692e6329cda21a02c13d10a7a
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-13 21:24:14 -0300

    new component

---
+++ src/components/Compo-Produtos/CompoSuinos/index.jsx
@@ -0,0 +1,72 @@
+import { Link } from 'react-router-dom'
+import suinos from '../../../imagens/icons-produtos-submenu/suinos.svg'
+import suinos1 from '../../../imagens/suinos/Pernil.png'
+import suinos2 from '../../../imagens/suinos/Sobre Paleta.png'
+import suinos3 from '../../../imagens/suinos/bisteca.png'
+import suinos4 from '../../../imagens/suinos/papada.png'
+import './compoSuinos.css'
+
+
+export default function CompoSuinos() {
+    return (
+        <div className="container-produtosSuinos">
+            <article className="text">
+                <img src={suinos} alt="icone suinos" />
+                <h1>Suinos</h1>
+            </article>
+
+            <section className="box-produto">
+                <section className="container">
+
+                    <div className="card-produto">
+                        <figure>
+                            <img src={suinos1} alt="imagem suinos" />
+                        </figure>
+                        <article>
+                            <p>Filé de Frango (peito s/ osso)</p>
+                        </article>
+                    </div>
+
+                    <div className="card-produto">
+                        <figure>
+                            <img src={suinos2} alt="imagem suinos" />
+                        </figure>
+                        <article>
+                            <p>Filé de Frango (peito s/ osso)</p>
+                        </article>
+                    </div>
+
+                    <div className="card-produto">
+                        <figure>
+                            <img src={suinos3} alt="imagem suinos" />
+                        </figure>
+                        <article>
+                            <p>Filé de Frango (peito s/ osso)</p>
+                        </article>
+                    </div>
+
+                    <div className="card-produto">
+                        <figure>
+                            <img src={suinos4} alt="imagem suinos" />
+                        </figure>
+                        <article>
+                            <p>Filé de Frango (peito s/ osso)</p>
+                        </article>
+                    </div>
+
+                </section>
+
+                <div className="button">
+                    <button>
+                        <p><Link to='/produtos/suinos'>Veja Mais</Link></p>
+                        
+                    </button>
+                </div>
+
+            </section>
+        </div>
+
+    )
+
+}
+
