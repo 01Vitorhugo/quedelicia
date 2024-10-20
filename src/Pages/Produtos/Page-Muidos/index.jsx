57bb013b1b74fbe9a9f9211bea21c1f54081b6e4
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-19 22:21:17 -0300

    adc content

--- src/Pages/Produtos/Page-Muidos/index.jsx
+++ src/Pages/Produtos/Page-Muidos/index.jsx
@@ -1,16 +1,45 @@
 import SubMenu from "../../../components/Compo-Produtos/SubMenuProdutos";
 import './muidos.css'
+import background from '../../../imagens/miudos/background.png'
+import data from "../../../components/Compo-Produtos/CardProduto/data";
 
 
-export default function Muidos(){
+export default function Muidos() {
 
-    return(
+    const bovinosProducts = data.cardData.filter(
+        (item) => item.category === 'muidos'
+    );
+
+    return (
         <>
-        <SubMenu/>
-        <section className='muidos'>
-            <h1>Muidos</h1>
+            <SubMenu />
+            <section className='muidos'>
+                <figure className='imagemBackground'>
+                    <img src={background} alt="Imagem muidos" />
+                </figure>
 
-        </section>
-    </>
+                <div className='box-produto-muidos'>
+                    {
+                        bovinosProducts.map((item) => {
+                            return (
+                                <section className='box-produto' key={item.id}>
+                                    <figure>
+                                        <img src={item.img} alt="imagem produto" />
+                                    </figure>
+
+                                    <article>
+                                        <p>{item.title}</p>
+
+                                    </article>
+                                </section>
+
+                            )
+
+                        })
+                    }
+
+                </div>
+            </section>
+        </>
     )
 }
