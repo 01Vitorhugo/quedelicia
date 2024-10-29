a078cac8e64be25601fb242553cf13a280add247
Vitor Hugo <vitorhugo0031@outlook.com>
2024-10-29 16:44:37 -0300

    new component

--- src/Pages/Produtos/Page-Frios/index.jsx
+++ src/Pages/Produtos/Page-Frios/index.jsx
@@ -1,14 +1,42 @@
 import './frios.css'
 import SubMenu from '../../../components/Compo-Produtos/SubMenuProdutos'
+import background from '../../../imagens/lacteos/background.png'
+import data from '../../../components/Compo-Produtos/CardProduto/data';
 
 export default function Frios(){
+    const friosProducts = data.cardData.filter(
+        (item) => item.category === 'frios'
+    );
 
     return(
         <>
             <SubMenu/>
             <section className='frios'>
-                <h1>Frios e laticínios</h1>
+            <figure className='imagemBackground'>
+                        <img src={background} alt="Imagem frios" />
+                    </figure>
 
+                <div className='box-produto-frios'>
+                    {
+                        friosProducts.map((item) => {
+                            return (
+                                <section className='box-produto' key={item.id}>
+                                    <figure>
+                                        <img src={item.img} alt="imagem produto" />
+                                    </figure>
+
+                                    <article>
+                                        <p>{item.title}</p>
+
+                                    </article>
+                                </section>
+
+                            )
+
+                        })
+                    }
+
+                </div>
             </section>
         </>
     )
