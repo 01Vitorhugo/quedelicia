dcdfcdebbd62811ba7344c7eadcfd29639c46479
Vitor Hugo <vitorhugo0031@outlook.com>
2024-11-05 19:01:24 -0300

    compo QuemSomos

---
+++ src/Pages/QuemSomos/index.jsx
@@ -0,0 +1,14 @@
+import NossaEstrutura from '../../components/Compo-QuemSomos/Estrutura'
+import CompoHistoria from '../../components/Compo-QuemSomos/Historia'
+import MisVisVal from '../../components/Compo-QuemSomos/MisVisVal'
+
+export default function QuemSomos(){
+
+    return(
+        <>
+        <CompoHistoria/>
+        <NossaEstrutura/>
+        <MisVisVal/>
+        </>
+    )
+}
